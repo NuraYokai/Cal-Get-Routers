3128c19f45b665ac4d98aa5edfe245ca79dcecf8
NuraYokai <yashrajgosavi2580@gmail.com>
2023-10-01 05:09:59 +0530

    feat: Implement theme for Android - Added colors, fonts, sizes, spacing
    Added necessary packages for styling:
    - styled-components@^5.3.9
    
    Implemented code formatter:
    - Added eslint@~8.37.0
    - Added prettier@~2.8.7
    - Added @react-native-community/eslint-config
    - Created .eslintrc file
    - Added "lint": "eslint . --ext .js" to the scripts in package.json

--- app/index.js
+++ app/index.js
@@ -1,13 +1,20 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "../src/infrastructure/theme";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <Text style={styles.title}>Hello World</Text>
+          <Text style={styles.subtitle}>
+            This is the first page of your app.
+          </Text>
+        </View>
       </View>
-    </View>
+    </ThemeProvider>
   );
 }
 
